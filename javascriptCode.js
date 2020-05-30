a23e122a31c274797812098eb9828003ddd1309f
RedcatPhoenix2 <62823569+RedcatPhoenix2@users.noreply.github.com>
2020-05-30 08:38:24 -0400

    Update javascriptCode.js

--- javascriptCode.js
+++ javascriptCode.js
@@ -7,3 +7,12 @@ var numberOfLives = 3;
   var isGameOver= false;
 isGameOver = true;
   var isAtEnd = false;
+  var characterName = "Me";
+characterName = "Phoenix"
+  var age = 13;
+
+let message = `Hi, Welcome to ${characterName}'s world. His age is ${age}`
+
+let length = characterName.length;
+let upperName = characterName.toUpperCase();
+characterName = characterName.toLowerCase();
