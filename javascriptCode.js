001467d113d55b8eba1894d6af64f5dc4ab612cf
RedcatPhoenix2 <62823569+RedcatPhoenix2@users.noreply.github.com>
2020-05-30 08:51:29 -0400

    Update javascriptCode.js

--- javascriptCode.js
+++ javascriptCode.js
@@ -16,3 +16,26 @@ let message = `Hi, Welcome to ${characterName}'s world. His age is ${age}`
 let length = characterName.length;
 let upperName = characterName.toUpperCase();
 characterName = characterName.toLowerCase();
+
+let p = characterName.slice(0,6);
+
+var level = 1
+var title - `Level ${level}`;
+
+var level = 2
+var title - `Level ${level}`;
+
+var level = 3
+var title - `Level ${level}`;
+
+var level = 4
+var title - `Level ${level}`;
+
+var level = 5
+var title - `Level ${level}`;
+
+health = health + 10; // 60
+health = health % 50; // 10
+// health % 2 == 0
+health = health ** 2 // 100
+health -= 20; // health = health - 20
