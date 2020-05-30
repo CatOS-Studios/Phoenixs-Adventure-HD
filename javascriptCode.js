c053356d21b06fa9aa2929be86877a28f3937d91
RedcatPhoenix2 <62823569+RedcatPhoenix2@users.noreply.github.com>
2020-05-30 08:29:17 -0400

    Update javascriptCode.js

--- javascriptCode.js
+++ javascriptCode.js
@@ -1,11 +1,9 @@
-// const, let, var
 const maxHealth = 100;
 maxHealth = 50; // Not allowed, will throw an error
 currentHealth = maxHealth
+//maxHealth = currentHealth;
+var numberOfLives = 3;
 
-if (true) {
   var isGameOver= false;
-  let isNotGameOver = true;
-}
-print(isGameOver); // false
-print(isNotGameOver); // Throws an error because we cannot access a let outside of the { }
+isGameOver = true;
+  var isAtEnd = false;
